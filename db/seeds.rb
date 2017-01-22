84704ea57fc61378c4bffa52a17289d796eafc9e
GiancarloVanoni <giancarlo.vanoni@gmail.com>
2017-01-22 19:04:19 -0300

    Adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pin.create!(
+  [
+    {
+      latitude: -34.896654,
+      longitude: -56.121117
+    },
+    {
+      latitude: -34.896408,
+      longitude: -56.121085
+    },
+    {
+      latitude: -34.895783,
+      longitude: -56.119315
+    },
+    {
+      latitude: -34.897085,
+      longitude: -56.120388
+    },
+    {
+      latitude: -34.903130,
+      longitude: -56.131653
+    },
+    {
+      latitude: -34.896408,
+      longitude: -56.121085
+    },
+    {
+      latitude: -34.856230,
+      longitude: -56.203514
+    },
+    {
+      latitude: -33.747384,
+      longitude: -61.968903
+    },
+    {
+      latitude: -33.747420,
+      longitude: -61.968603
+    },
+    {
+      latitude: -33.747884,
+      longitude: -61.968270
+    },
+    {
+      latitude: -33.747929,
+      longitude: -61.968688
+    },
+
+  ]
+)
